2b63a474343e09c20e9896a2c13dafdd9cc385e3
xerunium <xerunium@gmail.com>
2024-11-09 18:09:24 +0100

    feat : Crud Choix

--- back-skeleton/src/main/java/com/takima/backskeleton/choix/ChoixController.java
+++ back-skeleton/src/main/java/com/takima/backskeleton/choix/ChoixController.java
@@ -1,6 +1,7 @@
 package com.takima.backskeleton.choix;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,15 +13,26 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ChoixController {
 
-    private final ChoixDao choixDao;
+    private final ChoixService choixService;
 
     @GetMapping("")
     public List<Choix> findAll() {
-        return choixDao.findAll();
+        return choixService.findAll();
     }
 
     @GetMapping("/{id_choix}")
     public Optional<Choix> findById(@PathVariable Long id_choix) {
-        return choixDao.findById(id_choix);
+        return choixService.findById(id_choix);
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@RequestParam Long choix_id) {
+        choixService.deleteById(choix_id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Choix> updateChoix(@PathVariable Long id, @RequestBody Choix choixDetails) {
+        Choix updatedChoix = choixService.updateChoix(id, choixDetails);
+        return ResponseEntity.ok(updatedChoix);
     }
 }
--- back-skeleton/src/main/java/com/takima/backskeleton/choix/ChoixService.java
+++ back-skeleton/src/main/java/com/takima/backskeleton/choix/ChoixService.java
@@ -1,11 +1,51 @@
 package com.takima.backskeleton.choix;
 
+import com.takima.backskeleton.categorie.Categorie;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ChoixService {
 
     private final ChoixDao choixDao;
+
+    public List<Choix> findIncorrectChoixByCategorie(Categorie categorie, Long choixId, int nbrChoix) {
+        return choixDao.findIncorrectChoixByCategorie(categorie, choixId, nbrChoix);
+    }
+
+    public Optional<Choix> findByDescription(String description) {
+        return choixDao.findByDescription(description);
+    }
+
+    public Optional<Choix> findByImage(String image) {
+        return choixDao.findByImage(image);
+    }
+
+    public List<Choix> findAll() {
+        return choixDao.findAll();
+    }
+
+    public Optional<Choix> findById(Long choix_id) {
+        return choixDao.findById(choix_id);
+    }
+
+    public void deleteById(Long choix_id) {
+        choixDao.deleteById(choix_id);
+    }
+
+    @Transactional
+    public Choix updateChoix(Long id, Choix choixDetails) {
+        Choix choix = choixDao.findById(id)
+                .orElseThrow(() -> new RuntimeException("Choix not found with id " + id));
+
+        choix.setDescription(choixDetails.getDescription());
+        choix.setImage(choixDetails.getImage());
+        choix.setCategorie(choixDetails.getCategorie());
+        return choix;
+    }
 }
