2b699cc797db91b5c3da4087bdcc3e2a93670015
Xavier ANDREANI <xandreani@takima.fr>
2024-11-14 18:08:29 +0100

    background home qui avance

--- front-skeleton/src/app/home/home.component.ts
+++ front-skeleton/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from "@angular/core"
-import { Router } from "@angular/router"
-import { MatButtonToggleChange } from "@angular/material/button-toggle"
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "home",
@@ -8,27 +7,35 @@ import { MatButtonToggleChange } from "@angular/material/button-toggle"
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-  selectedDifficulty: string = "Simple"
-  username: string = ""
-  background: string = ""
+  selectedDifficulty: string = "Simple";
+  username: string = "";
+  background: string = "";
 
   constructor(private router: Router) {}
 
-  ngOnInit(): void {}
-
-  startQuiz() {
-    this.router.navigate(["/quiz"], { queryParams: { difficulty: this.selectedDifficulty, username: this.username } })
+  ngOnInit(): void {
+    this.updateBackground();  // Initialise l'arrière-plan au chargement
   }
 
-  onChange($event: MatButtonToggleChange) {
-    if (this.selectedDifficulty === "Simple") {
-      this.background = "https://chat-chou.com/wp-content/uploads/2023/10/chats-les-plus-mignons.jpg"
+  updateBackground() {
+    switch (this.selectedDifficulty) {
+      case "Simple":
+        this.background = "url('https://chat-chou.com/wp-content/uploads/2023/10/chats-les-plus-mignons.jpg')";
+        break;
+      case "Moyen":
+        this.background = "url('https://static.actu.fr/uploads/2021/03/13807881863-639a90e969-o.jpg')";
+        break;
+      case "Atroce":
+        this.background = "url('https://images.squarespace-cdn.com/content/v1/5ff618e1c6d974239d969567/230652e2-84b5-47f8-aa35-df2ad53ae763/joseph-z-whyd-god-create-hell-isnt-he-supposed-to-be-good.jpg')";
+        break;
+      default:
+        this.background = "";
     }
-    if (this.selectedDifficulty === "Moyen") {
-      this.background = "https://static.wikia.nocookie.net/something-wicked/images/7/73/Hell.jpg/revision/latest?cb=20171103211145"
-    }
-    if (this.selectedDifficulty === "Atroce") {
-      this.background = "https://static.actu.fr/uploads/2021/03/13807881863-639a90e969-o.jpg"
-    }
+  }
+
+  startQuiz() {
+    this.router.navigate(["/quiz"], {
+      queryParams: { difficulty: this.selectedDifficulty, username: this.username },
+    });
   }
 }
