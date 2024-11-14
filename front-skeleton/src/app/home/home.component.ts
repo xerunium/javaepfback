52e499b000c538af06b306eab51053e0b0ed4b82
Xavier ANDREANI <xandreani@takima.fr>
2024-11-14 17:56:57 +0100

    changements home

--- front-skeleton/src/app/home/home.component.ts
+++ front-skeleton/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core"
-import {Router} from "@angular/router";
+import { Router } from "@angular/router"
+import { MatButtonToggleChange } from "@angular/material/button-toggle"
 
 @Component({
   selector: "home",
@@ -7,14 +8,27 @@ import {Router} from "@angular/router";
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-  selectedDifficulty: string = 'Simple';
-  username: string = '' ;
-  constructor(private router : Router) {}
+  selectedDifficulty: string = "Simple"
+  username: string = ""
+  background: string = ""
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
   startQuiz() {
-    this.router.navigate(['/quiz'], { queryParams: { difficulty: this.selectedDifficulty, username: this.username } });
+    this.router.navigate(["/quiz"], { queryParams: { difficulty: this.selectedDifficulty, username: this.username } })
+  }
 
+  onChange($event: MatButtonToggleChange) {
+    if (this.selectedDifficulty === "Simple") {
+      this.background = "https://chat-chou.com/wp-content/uploads/2023/10/chats-les-plus-mignons.jpg"
+    }
+    if (this.selectedDifficulty === "Moyen") {
+      this.background = "https://static.wikia.nocookie.net/something-wicked/images/7/73/Hell.jpg/revision/latest?cb=20171103211145"
+    }
+    if (this.selectedDifficulty === "Atroce") {
+      this.background = "https://static.actu.fr/uploads/2021/03/13807881863-639a90e969-o.jpg"
+    }
   }
 }
